d7140c8274748f56ff8c6628840e94056f973449
Cameron Lyons <cameron.lyons2@gmail.com>
2022-02-11 18:17:53 -0600

    split longstr and avgstr

--- longstring.py
+++ longstring.py
@@ -4,15 +4,22 @@ from itertools import groupby
 
 def rle(message):
     '''Run-length encoding. Converts a string into a list of tuples, where each tuple contains the length of the run and the character.'''
-    return [(x, sum(1 for _ in y)) for x, y in groupby(message)]
+    return [(char, sum(1 for _ in substring)) for char, substring in groupby(message)]
 
 
-def rle_string(x):
-    rle_list = rle(x)
-    longstr = max(rle_list, key=len)
-    total_len = sum(len(s) for s in rle_list)
+def longstr_message(message):
+    '''return longest sequence of repeated characters in a string'''
+    rle_list = rle(message)
+    return max(rle_list, key=lambda x:x[1])
+
+
+def avgstr_message(message):
+    '''return average length of uninterrupted string of identical characters in a string'''
+    rle_list = rle(message)
+    total_len = sum(s[1] for s in rle_list)
     avgstr = float(total_len) / float(len(rle_list))
-    return zip(longstr, avgstr)
+    return avgstr
+
 
 def longstring(x, avg=False):
     '''Takes a matrix of item responses and, beginning with the second column (i.e., second item)
@@ -23,9 +30,7 @@ def longstring(x, avg=False):
     x a matrix of data (e.g. item responses)
     avg logical indicating whether to additionally return the average length of identical consecutive responses'''
 
-    output = rle_string(x)
-
     if avg:
-        return list(output)[1]
+        return [avgstr_message(message) for message in x]
     else:
-        return list(output)[0]
+        return [longstr_message(message) for message in x]
