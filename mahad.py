0665c87c41e48d14581c8b9931e1d06a16180948
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-11 10:33:01 -0500

    compute_mahalanobis

--- mahad.py
+++ mahad.py
@@ -12,3 +12,16 @@ def mahalanobis(x, mean, inv_cov_matrix):
     """Compute the Mahalanobis Distance"""
     x_minus_mean = x - mean
     return np.sqrt(x_minus_mean.T.dot(inv_cov_matrix).dot(x_minus_mean))
+
+
+def compute_mahalanobis(data):
+    """Computes Mahalanobis Distance for a dataset"""
+    mean = np.mean(data, axis=0)
+    cov_matrix = np.cov(data, rowvar=False)
+    inv_cov_matrix = np.linalg.inv(cov_matrix)
+
+    distances = []
+    for i in range(data.shape[0]):
+        d = mahalanobis(data[i, :], mean, inv_cov_matrix)
+        distances.append(d)
+    return distances
