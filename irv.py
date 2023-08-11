808fe49ad7e03c901a7e11baf451d66e51b87ed6
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-11 11:21:26 -0500

    irv

--- irv.py
+++ irv.py
@@ -7,3 +7,19 @@ questionnaire (usually the end). Whereas Dunn et al. (2018) propose to mark pers
 scores as outliers - reflecting straightlining responses, Marjanovic et al. (2015) propose to mark
 persons with high IRV scores - reflecting highly random responses
 """
+import numpy as np
+from typing import List, Union
+
+
+def irv(data: Union[List[List[float]], np.ndarray]) -> np.ndarray:
+    """
+    Calculate intra-individual response variability (IRV) for each individual.
+
+    Parameters:
+    - data: 2D list or numpy array where rows are individuals and columns are their responses.
+
+    Returns:
+    - A numpy array of IRV values for each individual.
+    """
+
+    return np.std(data, axis=1)
