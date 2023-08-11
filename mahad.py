9d1bc7d9f7446216322491263803569c05b161fc
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-11 10:36:50 -0500

    type hints

--- mahad.py
+++ mahad.py
@@ -6,22 +6,22 @@ Mahalanobis distance is calculated using a function which supports missing value
 
 import numpy as np
 from scipy.spatial import distance
+from typing import List, Tuple
 
 
-def mahalanobis(x, mean, inv_cov_matrix):
+def mahalanobis(x: np.ndarray, mean: np.ndarray, inv_cov_matrix: np.ndarray) -> float:
     """Compute the Mahalanobis Distance"""
     x_minus_mean = x - mean
     return np.sqrt(x_minus_mean.T.dot(inv_cov_matrix).dot(x_minus_mean))
 
 
-def compute_mahalanobis(data):
+def compute_mahalanobis(data: np.ndarray) -> List[float]:
     """Computes Mahalanobis Distance for a dataset"""
     mean = np.mean(data, axis=0)
     cov_matrix = np.cov(data, rowvar=False)
     inv_cov_matrix = np.linalg.inv(cov_matrix)
 
-    distances = []
-    for i in range(data.shape[0]):
-        d = mahalanobis(data[i, :], mean, inv_cov_matrix)
-        distances.append(d)
+    distances = [
+        mahalanobis(data[i, :], mean, inv_cov_matrix) for i in range(data.shape[0])
+    ]
     return distances
