64bc117048fb3baf220423bd53da80e13442efd8
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-11 11:25:51 -0500

    doc str

--- mahad.py
+++ mahad.py
@@ -1,8 +1,16 @@
-"""Find and graph Mahalanobis Distance (D) and flag potential outliers
+"""
+Find and graph Mahalanobis Distance (D) and flag potential outliers
 
 Takes a matrix of item responses and computes Mahalanobis D. Can additionally return a
 vector of binary outlier flags.
-Mahalanobis distance is calculated using a function which supports missing values."""
+Mahalanobis distance is calculated using a function which supports missing values.
+The Mahalanobis distance is a measure of the distance between a point P and a distribution D,
+introduced by P. C. Mahalanobis in 1936. It is a multi-dimensional generalization of the idea of
+measuring how many standard deviations away P is from the mean of D. This distance is zero if P is
+at the mean of D, and grows as P moves away from the mean along each principal component axis.
+The Mahalanobis distance is thus unitless and scale-invariant, and takes into account the
+correlations of the data set.
+"""
 
 import numpy as np
 from typing import List
