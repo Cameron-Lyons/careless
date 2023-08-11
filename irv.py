09901bc954a4f4a3c61c843ffd7838ed84181dbd
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-11 11:23:57 -0500

    add args

--- irv.py
+++ irv.py
@@ -8,18 +8,39 @@ scores as outliers - reflecting straightlining responses, Marjanovic et al. (201
 persons with high IRV scores - reflecting highly random responses
 """
 import numpy as np
-from typing import List, Union
+from typing import List, Union, Optional
 
 
-def irv(data: Union[List[List[float]], np.ndarray]) -> np.ndarray:
+def irv(
+    x: Union[List[List[float]], np.ndarray],
+    na_rm: bool = True,
+    split: bool = False,
+    num_split: Optional[int] = 1,
+) -> np.ndarray:
     """
     Calculate intra-individual response variability (IRV) for each individual.
 
     Parameters:
-    - data: 2D list or numpy array where rows are individuals and columns are their responses.
+    - x: A matrix of data where rows are individuals and columns are their responses.
+    - na_rm: Boolean indicating whether to ignore missing values (np.nan) during computation.
+    - split: Boolean indicating whether to additionally calculate the IRV on subsets of columns.
+    - num_split: Number of subsets the data is to be split into if 'split' is True.
 
     Returns:
     - A numpy array of IRV values for each individual.
     """
 
-    return np.std(data, axis=1)
+    if na_rm:
+        x = np.nan_to_num(x, nan=np.mean(x, axis=1, keepdims=True))
+
+    irvs = np.std(x, axis=1)
+
+    if split:
+        chunk_size = x.shape[1] // num_split
+        irvs_splits = [
+            np.std(x[:, i : i + chunk_size], axis=1)
+            for i in range(0, x.shape[1], chunk_size)
+        ]
+        irvs = np.mean(irvs_splits, axis=0)
+
+    return irvs
