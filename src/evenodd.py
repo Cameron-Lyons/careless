1dd94ee4ea4a3075178730b7b6ef53f2c0d336ac
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-21 23:21:44 -0500

    fix comments

--- src/evenodd.py
+++ src/evenodd.py
@@ -18,40 +18,31 @@ def evenodd(
     - A numpy array of even-odd consistency scores or a tuple of scores and diagnostic values.
     """
 
-    # Ensure the input is a numpy array
     x = np.array(x)
 
-    # Variable to store the average correlations for each individual
     avg_correlations = np.zeros(x.shape[0])
 
-    # Diagnostic values to store the number of available even/odd pairs for each individual
     diag_vals = np.zeros(x.shape[0], dtype=int)
 
     start_col = 0
     for factor_size in factors:
         end_col = start_col + factor_size
 
-        # Split the matrix into even and odd columns
         even_cols = x[:, start_col:end_col:2]
         odd_cols = x[:, (start_col + 1) : end_col : 2]
 
-        # Compute pairwise correlations for every individual between even and odd columns
-        # Then average them out for the factor and add to the avg_correlations
         correlations = np.array(
             [np.corrcoef(e, o)[0, 1] for e, o in zip(even_cols, odd_cols)]
         )
 
-        # Handle any NaN values due to lack of variance and set them to 0
         correlations[np.isnan(correlations)] = 0
 
         avg_correlations += correlations
 
-        # Update diagnostic values
         diag_vals += np.minimum(even_cols.shape[1], odd_cols.shape[1])
 
         start_col = end_col
 
-    # Average the correlations across all factors
     avg_correlations /= len(factors)
 
     if diag:
