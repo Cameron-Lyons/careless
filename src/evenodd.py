54b74babf46a0723793ddad0f4247c3ce7100a2f
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-29 16:16:40 -0500

    lint

--- src/evenodd.py
+++ src/evenodd.py
@@ -4,6 +4,15 @@ import numpy as np
 from typing import List, Union, Tuple
 
 
+def calculate_correlations(even_cols: np.ndarray, odd_cols: np.ndarray) -> np.ndarray:
+    """Calculates correlations between even and odd columns."""
+    correlations = np.array(
+        [np.corrcoef(e, o)[0, 1] for e, o in zip(even_cols, odd_cols)]
+    )
+    correlations[np.isnan(correlations)] = 0
+    return correlations
+
+
 def evenodd(
     x: Union[List[List[float]], np.ndarray], factors: List[int], diag: bool = False
 ) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
@@ -20,33 +29,21 @@ def evenodd(
     """
 
     x = np.array(x)
-
+    num_factors = len(factors)
     avg_correlations = np.zeros(x.shape[0])
-
     diag_vals = np.zeros(x.shape[0], dtype=int)
 
     start_col = 0
     for factor_size in factors:
         end_col = start_col + factor_size
-
         even_cols = x[:, start_col:end_col:2]
         odd_cols = x[:, (start_col + 1) : end_col : 2]
 
-        correlations = np.array(
-            [np.corrcoef(e, o)[0, 1] for e, o in zip(even_cols, odd_cols)]
-        )
-
-        correlations[np.isnan(correlations)] = 0
-
-        avg_correlations += correlations
-
+        avg_correlations += calculate_correlations(even_cols, odd_cols)
         diag_vals += np.minimum(even_cols.shape[1], odd_cols.shape[1])
-
         start_col = end_col
 
-    avg_correlations /= len(factors)
+    if num_factors:
+        avg_correlations /= num_factors
 
-    if diag:
-        return avg_correlations, diag_vals
-    else:
-        return avg_correlations
+    return (avg_correlations, diag_vals) if diag else avg_correlations
