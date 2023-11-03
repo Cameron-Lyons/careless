78b599c84296fb5df8751b7da0a7d36125189e48
Cameron Lyons <cameron.lyons2@gmail.com>
2023-11-03 11:19:47 -0500

    fix comment

--- tests/unit.py
+++ tests/unit.py
@@ -160,7 +160,6 @@ class TestEvenOddFunction(unittest.TestCase):
 
 class TestPsychometricFunctions(unittest.TestCase):
     def setUp(self):
-        # A toy dataset
         self.data = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]])
 
     def test_psychsyn_basic(self):
